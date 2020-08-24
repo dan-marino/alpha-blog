62ff17b8e2d3effc3c4e01e6a0dcb889fe0c4dc2
Daniel Marino <marinodanielj@hotmail.com>
2020-08-24 15:13:54 -0400

    can delete article. added links to pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :create, :new, :edit, :update]
+  resources :articles
   # get 'articles', to: 'articles#index', as: 'articles'
   # get 'articles/new', to: 'articles#new', as: 'new_article'
   # get 'articles/:id', to: 'articles#show', as: 'article'
   # post 'articles', to: 'articles#create'
   # get 'articles/:id/edit', to: 'edit_article'
   # patch 'articles/:id', to: 'articles#update'
+  # put 'articles/:id', to: 'articles#update'
+  # delete 'articles/:id', to: 'articles#destroy'
 end
