6e66b8e87f5648e40042f409a5ed5f71948b8963
Daniel Marino <marinodanielj@hotmail.com>
2020-08-24 14:18:13 -0400

    can add new articles. flash messages enabled

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,24 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new()
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render new_article_path
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
-  # resources :articles, only: [:show, :index]
-  get 'articles/:id', to: 'articles#show'
-  get 'articles', to: 'articles#index'
+  resources :articles, only: [:show, :index, :create, :new]
+  # get 'articles', to: 'articles#index', as: 'articles'
+  # get 'articles/new', to: 'articles#new', as: 'new_article'
+  # get 'articles/:id', to: 'articles#show', as: 'article'
+  # post 'articles', to: 'articles#create'
 end
