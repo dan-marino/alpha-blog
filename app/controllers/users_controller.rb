dfa42c443e81df466dd128e236fcb20ef63b40ab
Daniel Marino <marinodanielj@hotmail.com>
2020-08-26 17:44:45 -0400

    Fixed more bugs related to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   def index
     @users = User.all
   end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 
   def new
     @user = User.new
+  end
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Successfully edited #{@user.username}'s profile'"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def create
@@ -22,9 +32,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy; end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
