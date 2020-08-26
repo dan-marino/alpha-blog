61cf963bf6a6ca36db41f38de81754a944339e4a
Daniel Marino <marinodanielj@hotmail.com>
2020-08-26 17:23:57 -0400

    Creating users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(param[:id])
+  end
+
   def new
     @user = User.new
   end
