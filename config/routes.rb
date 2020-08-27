ed5b2391a05f5f5bc0a4e88817c6d1cfccb0a9fd
Daniel Marino <marinodanielj@hotmail.com>
2020-08-26 20:03:31 -0400

    created login form for user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: "sessions#create"
+  delete 'login', to: 'sessions#destroy'
 
   resources :articles
   # get 'articles', to: 'articles#index', as: 'articles'
